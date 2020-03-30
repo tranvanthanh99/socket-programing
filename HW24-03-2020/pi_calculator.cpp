cddaebc59a7f49d3aaefe810df296a70a5835aab
tranvanthanh99 <thanhvantran99@gmail.com>
2020-03-31 00:21:35 +0700

    HW24-03-2020

--- HW24-03-2020/pi_calculator.cpp
+++ HW24-03-2020/pi_calculator.cpp
@@ -1,30 +1,48 @@
 #include <stdio.h>
+#include <winsock.h>
 
-int main()
+static long num_steps = 100000;
+static double total_sum = 0;
+const int numberOfThread = 8;
+const int jump_step = (num_steps / numberOfThread);
 
-{
-	static long num_steps = 100000;
+double pi;
+double step = 1 / ((double)num_steps);
+
+CRITICAL_SECTION cs;
+
+DWORD WINAPI solve(LPVOID arg) {
+	double x, sum = 0.0;
+	int index = *(int*)arg;
 	
-	
-	double step, pi;
-    int i;
+	for (int i = index - jump_step; i < index; i++) {
+		x = (i + 0.5)*step;
+		sum = sum + 4.0/(1.0 + x*x);
+	}
 
-    double x, sum = 0.0;
+	EnterCriticalSection(&cs);
+	total_sum += sum;
+	LeaveCriticalSection(&cs);
+	return 0;
+}
 
- 
+main() {
+	HANDLE hThread[numberOfThread];
+	InitializeCriticalSection(&cs);
+	int params[numberOfThread];
+	for (int i = 0; i < numberOfThread; i++) {
+		params[i] = (jump_step) * (i + 1);
+		// = { 12500, 25000, 37500, 50000, 62500, 75000 , 87500, 100000 };
+	}
 
-    step = 1.0 / (double)num_steps;
+	for (int i = 0; i < numberOfThread; i++) {
+		hThread[i] = CreateThread(0, 0, solve, &params[i], 0, 0);
+	}
 
-    for (i = 0; i < num_steps; i++) {
+	WaitForMultipleObjects(numberOfThread, hThread, TRUE, INFINITE);
 
-        x = (i + 0.5) * step;
+	pi = step * total_sum;
+	printf("Pi = %f\n", pi);
 
-        sum = sum + 4.0 / (1.0 + x * x);
-
-    }
-
-    pi = step * sum;
-
-    printf("Pi = % f\n", pi);
-
+	DeleteCriticalSection(&cs);
 }
